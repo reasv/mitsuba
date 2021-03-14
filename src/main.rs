bde69353fed4f31ebc5e48e1ac2e2f33e45d5797
reasv <7143787+reasv@users.noreply.github.com>
2021-03-14 19:42:38 +0100

    Remove comments in main.rs

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,6 @@ mod api;
 use api::web_main;
 use board_archiver::{Archiver};
 use http::HttpClient;
-// use models::Board;
 
 #[tokio::main]
 async fn main() {
@@ -25,12 +24,9 @@ async fn main() {
     env_logger::init();
     let client = Archiver::new(HttpClient::new(nonzero!(120u32), nonzero!(10u32), 200, 800, 600));
 
-    // client.add_board(Board{ name: "po".to_string(), wait_time: 10, full_images: false, last_modified: 0, archive: true}).await.unwrap();
-    // client.add_board(Board{ name: "vip".to_string(), wait_time: 10, full_images: false, last_modified: 0, archive: true}).await.unwrap();
+    // use models::Board;
+    // client.set_board(Board{ name: "w".to_string(), wait_time: 10, full_images: false, last_modified: 0, archive: true}).await.unwrap();
     
     client.run_archivers().await.unwrap();
-    // loop {
-    //     tokio::time::sleep(Duration::from_secs(5)).await;
-    // }
     web_main().unwrap();
 }
