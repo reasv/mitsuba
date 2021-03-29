e8a7a4da7efc4280ff627bfdff9cbd8f124a6db9
reasv <7143787+reasv@users.noreply.github.com>
2021-03-30 00:40:07 +0200

    Add page value for image jobs

--- migrations/20210329215000_create_image_backlog.sql
+++ migrations/20210329215000_create_image_backlog.sql
@@ -6,7 +6,9 @@ CREATE TABLE image_backlog (
   thumbnail_url TEXT NOT NULL,
   filename TEXT NOT NULL,
   thumbnail_filename TEXT NOT NULL,
+  page INTEGER NOT NULL,
   file_sha256 TEXT NOT NULL,
   thumbnail_sha256 TEXT NOT NULL,
   UNIQUE(board, no)
 );
+CREATE INDEX page_index ON posts (page);
