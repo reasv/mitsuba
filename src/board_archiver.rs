75c4df90c7cda0875b39e7e6bd0c9dadecbb9786
reasv <7143787+reasv@users.noreply.github.com>
2021-04-11 06:56:18 +0200

    Board rescan based on changes

--- src/board_archiver.rs
+++ src/board_archiver.rs
@@ -13,38 +13,50 @@ impl Archiver {
     pub async fn get_board_pages(&self, board: &String) -> Result<Vec<ThreadsPage>, bool> {
         self.http_client.fetch_json::<Vec<ThreadsPage>>(&get_board_page_api_url(board)).await
     }
-    pub async fn push_new_threads(&self, board: &String) -> anyhow::Result<(), bool> {
+    pub async fn push_new_threads(&self, board: &String) -> anyhow::Result<u64, bool> {
         let mut pages = self.get_board_pages(board).await?;
+        let mut added_jobs: u64 = 0;
         while let Some(mut page) = pages.pop() { // pop lets us iterate in reverse, we want threads about to die to get fetched first
             // eventually we should implement a formal priority queue system especially for images
             while let Some(mut thread_info) = page.threads.pop() {
                 thread_info.board = board.clone();
                 thread_info.page = page.page as i32;
-                self.db_client.insert_thread_job(&thread_info).await
+                let job_opt = self.db_client.insert_thread_job(&thread_info).await
                 .map_err(|e| {error!("Error inserting thread job into database: {}", e); false})?;
+                if job_opt.is_some() {
+                    added_jobs +=1;
+                }
             }
         }
-        Ok(())
+        Ok(added_jobs)
     }
-    pub async fn board_cycle(&self) -> anyhow::Result<(), bool> {
+    pub async fn board_cycle(&self) -> anyhow::Result<u64, bool> {
+
         let boards = self.db_client.get_all_boards().await
         .map_err(|e| {error!("Error getting board settings from database: {}", e); false})?;
+        let mut added_jobs: u64 = 0;
         for board in boards {
             if !board.archive {
                 continue;
             }
-            self.push_new_threads(&board.name).await?;
+            added_jobs += self.push_new_threads(&board.name).await?;
         }
-        Ok(())
+        Ok(added_jobs)
     }
     pub fn run_board_cycle(&self) -> tokio::task::JoinHandle<()> {
         let c = self.clone();
         tokio::task::spawn(async move {
             loop {
                 let s = Instant::now();
-                c.board_cycle().await.ok();
+                let add_opt = c.board_cycle().await.ok();
                 histogram!("boards_scan_duration", s.elapsed().as_millis() as f64);
-                tokio::time::sleep(Duration::from_secs(10)).await;
+                if let Some(added_jobs) = add_opt {
+                    if added_jobs == 0 { // No new changes
+                        tokio::time::sleep(Duration::from_secs(10)).await;
+                    } else {
+                        tokio::time::sleep(Duration::from_secs(1)).await;
+                    }
+                }
             }
         })
     }
