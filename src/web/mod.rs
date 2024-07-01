0e1038c4b4260d299b710b3ab13decb75195f909
reasv <7143787+reasv@users.noreply.github.com>
2024-07-01 14:26:22 +0200

    Add gzip compression to actix-web

--- src/web/mod.rs
+++ src/web/mod.rs
@@ -1,3 +1,4 @@
+use actix_web::middleware;
 #[allow(unused_imports)]
 use log::{info, warn, error, debug};
 
@@ -27,6 +28,7 @@ pub async fn web_main() -> std::io::Result<()> {
         .app_data(web::Data::new(dbc.clone()))
         .app_data(handlebars_ref.clone())
         .wrap(NormalizePath::default())
+        .wrap(middleware::Compress::default())
         .service(api::get_index)
         .service(api::get_thread)
         .service(api::get_post)
