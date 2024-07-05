86d493d11963d14f9dc3e4f56495d7653881e7e1
reasv <7143787+reasv@users.noreply.github.com>
2024-07-05 04:10:06 +0200

    Add generic errors

--- src/web/auth.rs
+++ src/web/auth.rs
@@ -1,25 +1,89 @@
 use std::pin::Pin;
 use actix_session::SessionExt;
-use actix_web::{Error, FromRequest, web};
+use actix_web::{Error, FromRequest, web, ResponseError, HttpResponse};
 use futures::Future;
 use log::error;
+use serde::Serialize;
 use std::marker::PhantomData;
 
 use crate::{archiver::Archiver, models::{UserRole, User}};
 
-pub struct AuthUser<R: RoleCheck = AnyRole>{
+pub struct AuthUser<R: RoleCheck = AnyRole, E: RoleCheckError = JSONRCError>{
     pub name: String,
     pub role: UserRole,
-    _marker: PhantomData<R>
+    _marker: PhantomData<R>,
+    _error_marker: PhantomData<E>
 }
 
-impl<R: RoleCheck> AuthUser<R> {
-    pub fn anonymous() -> AuthUser<R> {
+impl<R: RoleCheck, E: RoleCheckError> AuthUser<R, E> {
+    pub fn anonymous() -> AuthUser<R, E> {
         AuthUser {
             name: "Anonymous".to_string(),
             role: UserRole::Anonymous,
-            _marker: PhantomData
+            _marker: PhantomData,
+            _error_marker: PhantomData
         }
+    }
+}
+
+pub trait RoleCheckError {
+    fn not_authenticated() -> actix_web::Error;
+    fn not_authorized(role: &UserRole) -> actix_web::Error;
+}
+
+pub struct JSONRCError;
+pub struct TextRCError;
+
+impl RoleCheckError for TextRCError {
+    fn not_authenticated() -> actix_web::Error {
+        actix_web::error::ErrorUnauthorized("User must be authenticated")
+    }
+
+    fn not_authorized(role: &UserRole) -> actix_web::Error {
+        actix_web::error::ErrorForbidden(format!("User role not authorized for this action: {:?}", role))
+    }
+}
+
+impl RoleCheckError for JSONRCError {
+    fn not_authenticated() -> actix_web::Error {
+        JSONError {
+            error: "Not Authenticated".to_string(),
+            code: actix_web::http::StatusCode::UNAUTHORIZED,
+            message: "User must be authenticated".to_string()
+        }.into()
+    }
+
+    fn not_authorized(role: &UserRole) -> actix_web::Error {
+        JSONError {
+            error: "Not Authorized".to_string(),
+            code: actix_web::http::StatusCode::FORBIDDEN,
+            message: format!("User role not authorized for this action: {:?}", role)
+        }.into()
+    }
+}
+
+#[derive(Debug, Serialize)]
+struct JSONError {
+    error: String,
+    message: String,
+    #[serde(skip)]
+    code: actix_web::http::StatusCode
+}
+
+impl std::fmt::Display for JSONError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}: {}", self.error, self.message)
+    }
+}
+
+impl ResponseError for JSONError {
+    fn error_response(&self) -> HttpResponse {
+        HttpResponse::build(self.status_code())
+            .json(self)
+    }
+
+    fn status_code(&self) -> actix_web::http::StatusCode {
+        self.code
     }
 }
 
@@ -61,17 +125,18 @@ pub trait RoleCheck {
 }
 
 // implement from User for session user
-impl<R: RoleCheck> From<User> for AuthUser<R> {
+impl<R: RoleCheck, E: RoleCheckError> From<User> for AuthUser<R, E> {
     fn from(user: User) -> Self {
         AuthUser {
             name: user.name,
             role: user.role,
-            _marker: PhantomData
+            _marker: PhantomData,
+            _error_marker: PhantomData
         }
     }
 }
 
-impl<R: RoleCheck> FromRequest for AuthUser<R> {
+impl<R: RoleCheck, E: RoleCheckError> FromRequest for AuthUser<R, E> {
     type Error = Error;
     type Future = Pin<Box<dyn Future<Output = Result<Self, Self::Error>>>>;
 
@@ -84,9 +149,9 @@ impl<R: RoleCheck> FromRequest for AuthUser<R> {
             .ok().flatten();
         Box::pin(async move {
             let anonymous_result = if R::is_allowed(&UserRole::Anonymous) {
-                Ok(AuthUser::<R>::anonymous())
+                Ok(AuthUser::<R, E>::anonymous())
             } else {
-                Err(actix_web::error::ErrorForbidden("User Must be authenticated"))
+                Err(E::not_authenticated())
             };
             match (archiver_data, username_opt) {
                 (Some(data), Some(username)) => {
@@ -96,11 +161,11 @@ impl<R: RoleCheck> FromRequest for AuthUser<R> {
                         .map_err(|e| error!("Could not retrieve session user data from db (user: {}): {}", username, e))
                         .ok()
                         .flatten() {
-                            let auth_user: AuthUser<R> = user_struct.into();
+                            let auth_user: AuthUser<R, E> = user_struct.into();
                             if R::is_allowed(&auth_user.role) {
                                 return Ok(auth_user)
                             } else {
-                                return Err(actix_web::error::ErrorForbidden("User role not allowed"));
+                                return Err(E::not_authorized(&auth_user.role));
                             }
                         }
                     anonymous_result
